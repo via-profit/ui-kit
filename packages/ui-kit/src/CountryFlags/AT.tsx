d13878d6608564d31d7c4c0fbb9e1ae32f03473c
Vasily Novosad <73801977+vasily-novosad@users.noreply.github.com>
2023-10-10 20:16:13 +0500

    WIP

--- packages/ui-kit/src/CountryFlags/AT.tsx
+++ packages/ui-kit/src/CountryFlags/AT.tsx
@@ -1,12 +1,20 @@
-
 import * as React from 'react';
 
 const AT: React.ForwardRefRenderFunction<SVGSVGElement, React.SVGProps<SVGSVGElement>> = (
   props,
   ref,
 ) => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 513 342" width="1.5em" height="1em" {...props} ref={ref}><path fill="#FFF" d="M0 114h513v114H0z"/><path fill="#D80027" d="M0 0h513v114H0zM0 228h513v114H0z"/></svg>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 513 342"
+    width="1.5em"
+    height="1em"
+    {...props}
+    ref={ref}
+  >
+    <path fill="#FFF" d="M0 114h513v114H0z" />
+    <path fill="#D80027" d="M0 0h513v114H0zM0 228h513v114H0z" />
+  </svg>
 );
 
 export default React.forwardRef(AT);
-    
--- packages/ui-kit/src/CountryFlags/MH.tsx
+++ packages/ui-kit/src/CountryFlags/MH.tsx
@@ -1,12 +1,25 @@
-
 import * as React from 'react';
 
 const MH: React.ForwardRefRenderFunction<SVGSVGElement, React.SVGProps<SVGSVGElement>> = (
   props,
   ref,
 ) => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 513 342" width="1.5em" height="1em" {...props} ref={ref}><path fill="#1E509C" d="M0 0h513v342H0z"/><path fill="#FFF" d="M513 176.1V81.3L0 342l513-165.9z"/><path fill="#F18D36" d="M513 0v81.3L0 342 513 0z"/><path fill="#FFF" d="m126.7 99.3-7.8-54.1-7.7 54.1-20.8-23.7 10.7 31.6-31.5-10.7 23.6 20.8-54.1 7.7 54.1 7.7-23.6 20.8 31.5-10.7-10.7 31.6 20.8-23.7 7.7 54.1 7.8-54.1 20.7 23.7-10.6-31.6 31.5 10.7-23.6-20.8 54-7.7-54-7.7 23.6-20.8-31.5 10.7 10.6-31.6z"/></svg>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 513 342"
+    width="1.5em"
+    height="1em"
+    {...props}
+    ref={ref}
+  >
+    <path fill="#1E509C" d="M0 0h513v342H0z" />
+    <path fill="#FFF" d="M513 176.1V81.3L0 342l513-165.9z" />
+    <path fill="#F18D36" d="M513 0v81.3L0 342 513 0z" />
+    <path
+      fill="#FFF"
+      d="m126.7 99.3-7.8-54.1-7.7 54.1-20.8-23.7 10.7 31.6-31.5-10.7 23.6 20.8-54.1 7.7 54.1 7.7-23.6 20.8 31.5-10.7-10.7 31.6 20.8-23.7 7.7 54.1 7.8-54.1 20.7 23.7-10.6-31.6 31.5 10.7-23.6-20.8 54-7.7-54-7.7 23.6-20.8-31.5 10.7 10.6-31.6z"
+    />
+  </svg>
 );
 
 export default React.forwardRef(MH);
-    
