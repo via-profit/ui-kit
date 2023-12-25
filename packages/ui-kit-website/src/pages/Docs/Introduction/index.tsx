a7ba17080640275e0d79fb8a2d8f853eefaf7f8c
Vasily Novosad <delhsmail@gmail.com>
2023-12-25 18:15:26 +0500

    Docs

--- packages/ui-kit-website/src/pages/Docs/Introduction/index.tsx
+++ packages/ui-kit-website/src/pages/Docs/Introduction/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import MarkdownRender from '~/components/RenderMarkdown';
+import Surface from '@via-profit/ui-kit/src/Surface';
 
 import content from '@via-profit/ui-kit/docs/README.md';
 
 const Introduction: React.FC = () => {
   const modifiedContent = content.replace(/\.\//g, './docs/');
 
-  return <MarkdownRender>{modifiedContent}</MarkdownRender>;
+  return (
+    <Surface>
+      <MarkdownRender>{modifiedContent}</MarkdownRender>
+    </Surface>
+  );
 };
 
 export default Introduction;
